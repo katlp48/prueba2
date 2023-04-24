2a1bb01522d871dde3fb483974187e14492ef25c
JosephCcahuanaV <u201717928@upc.edu.pe>
2023-04-23 23:49:07 -0500

    Total Requisitos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RequisitosComponent } from './components/requisitos/requisitos.component';
+import { RequisitosCreaeditaComponent } from './components/requisitos/requisitos-creaedita/requisitos-creaedita.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'requisitos', component: RequisitosComponent, children: [
+      { path: 'nuevo', component: RequisitosCreaeditaComponent },
+      { path: 'edicion/:id', component: RequisitosCreaeditaComponent }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {HttpClientModule} from '@angular/common/http';
@@ -9,10 +8,17 @@ import { MatTableModule } from '@angular/material/table';
 import { FormsModule,ReactiveFormsModule } from '@angular/forms';
 import {MatInputModule} from '@angular/material/input';
 import {MatButtonModule} from '@angular/material/button';
+import { RequisitosComponent } from './components/requisitos/requisitos.component';
+import { RequisitosListarComponent } from './components/requisitos/requisitos-listar/requisitos-listar.component';
+import { RequisitosCreaeditaComponent } from './components/requisitos/requisitos-creaedita/requisitos-creaedita.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RequisitosComponent,
+    RequisitosListarComponent,
+    RequisitosCreaeditaComponent,
+
   ],
   imports: [
     BrowserModule,
