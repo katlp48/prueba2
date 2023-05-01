17304b5ce58b076013850239be2e2aa9d9d7518c
katlp48 <130487303+katlp48@users.noreply.github.com>
2023-05-01 09:48:01 -0500

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import {  NavigationEnd, Router } from '@angular/router';
 import { filter } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'demo';
   open:boolean = true
-
-  constructor(private router:Router)
+  constructor(private router:Router,private http: HttpClient)
   {
     this.router.events.pipe(
       filter(event => event instanceof NavigationEnd)
@@ -24,6 +24,27 @@ export class AppComponent {
       }
     })
 
+  }
+  ngOnInit() {
+    this.http.get('http://localhost:5000/Empresa').subscribe(data => {
+      console.log(data);
+    });
+
+    this.http.get('http://localhost:5000/Requisitos').subscribe(data => {
+      console.log(data);
+    });
+    this.http.get('http://localhost:5000/Usuario').subscribe(data => {
+      console.log(data);
+    });
+    this.http.get('http://localhost:5000/Institucion_Educativa').subscribe(data => {
+      console.log(data);
+    });
+    this.http.get('http://localhost:5000/Carreras').subscribe(data => {
+      console.log(data);
+    });
+    this.http.get('ttp://localhost:5000/Calificacion').subscribe(data => {
+      console.log(data);
+    });
   }
 }
 
